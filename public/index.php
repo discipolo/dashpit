0376f79917cef62dce7fa168496c3e4b45fe6e7c
discipolo <discipolo@105174.no-reply.drupal.org>
2016-08-13 12:09:22 +0200

    parse multiple json files for links through different templates Issue #10

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 
 <?php
+
+
+
 // Autoload dependencies
 // https://getcomposer.org/doc/01-basic-usage.md#autoloading
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use dashpit\config;
 use Symfony\Component\Yaml\Yaml;
@@ -10,31 +13,32 @@ use Symfony\Component\Yaml\Exception\ParseException;
 
 // load config
 $config = new config();
-$settings = $config->getConfig('config.yml');
+$settings = $config->getConfig('../config.yml');
 $theme_directory = $settings['theme'];
 
 
 // initialize presentation layer
-$loader = new Twig_Loader_Filesystem($theme_directory, './templates');
+$loader = new Twig_Loader_Filesystem($theme_directory, '../templates');
 $loader->addPath($theme_directory);
-$loader->addPath('./templates');
+$loader->addPath('../templates');
 $twig = new Twig_Environment($loader, array(
     // 'cache' => 'compilation_cache',
 ));
 
 
 $link_lists = $settings['linklists'];
-foreach ($link_lists as $listsource => $link_list) {
+
+foreach ($link_lists as $listsource => $link_list_settings) {
     // get data
-    $parsed_linklist = $config->getConfig($link_list['source']);
+    $linklist = new \dashpit\LinkList($link_list_settings);
+    $linklist->init($loader);
+    $parsed_linklist = $linklist->getParsedList();
+    $linklist->render($parsed_linklist, $listsource);
+    //$parsed_linklist = $config->getConfig($link_list['source']);
     // Debug
     //echo 'DEBUG:';
     //var_dump($link_list);
-    echo $twig->render('linklist.html', array(
-        'sitename' => 'dashpit',
-        'linklist' => $parsed_linklist,
-        'listsource' => $listsource
-    ));
+
 }
 
 
